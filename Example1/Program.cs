827827333371efa61af0e0c979d54f41d3d21020
tharick <tharick@mail.usf.edu>
2019-01-31 18:01:02 -0500

    Change

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -6,7 +6,24 @@ namespace Example1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine(" What is your name?");
+            // This variable will gather data from user input
+
+            string Username = Console.ReadLine();
+            // Stores user input as sting called "Username"
+
+            Console.WriteLine("Hello " + Username + "!");
+            // Displays users input back to screen
+            Console.WriteLine("Press any key to exit the program and try again ...");
+            Console.ReadKey(true);
+
+
+
+
+
+
+        }// End of main
+
+    }// End of class
+
+} // End namespace 
